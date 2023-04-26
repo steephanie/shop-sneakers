5c37da0fd7ac98852f5afd396e3da0e932bac99c
Stephanie Nascimento <54187353+steephanie@users.noreply.github.com>
2023-04-26 17:50:52 -0300

    chore: refinement

--- App.tsx
+++ App.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import 'intl'
-import 'intl/locale-data/jsonp/pt-BR'
-
 import FlashMessage from 'react-native-flash-message'
-
 import { ThemeProvider } from 'styled-components'
-
 import { CartProvider } from './src/components/ContextMessage'
 import { appTheme } from './src/styles/theme'
 import { Routes } from './src/routes'
-import Skeleton from 'react-loading-skeleton'
 
 const App = () => {
   return (
